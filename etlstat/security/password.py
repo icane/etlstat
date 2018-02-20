d204e0646e7cfb13d9fb5210b5e3a057b83b36e8
Alberto Lezcano Lastra <lezcano_a@cantabria.es>
2018-02-20 18:28:07 +0100

    Add parameters to password generation method

--- etlstat/security/password.py
+++ etlstat/security/password.py
@@ -2,24 +2,48 @@ import string
 from random import *
 
 
-def database_password(minlength, maxlength):
+def generate_password(
+        min_length,
+        max_length,
+        first_letter=False,
+        upper_case=False,
+        lower_case=False,
+        punctuation=True,
+        digits=True
+):
     """
-    Generación de una contraseña aleatoria, formada por una letra inicial más una combinación de letras,
-    dígitos y signos de puntuación
-    :param minlength: longitud mínima de la contraseña
-    :param maxlength: longitud máxima de la contraseña
-    :return: contraseña generada
+    Generates a random password.
+    :param min_length: (:int:): password min. length
+    :param max_length: (:int:): password max. length
+    :param first_letter: (:bool:): True if first character must be a letter
+    :param upper_case: (:bool:): True if only uppercase letters
+    :param lower_case: (:bool:): True if only lowercase letters
+    :param punctuation: (:bool:): by default, use punctuation signs
+    :param digits: (:bool:): by default, use digits
+    :return: (:obj:str:): generated password
     """
-    if (minlength > maxlength):
-        min = maxlength
-        max = minlength
-    else:
-        min = minlength
-        max = maxlength
 
-    # in Oracle, first character must be a letter
-    first = choice(string.ascii_letters)
-    # valid characters: letters (lower and upper case), punctuation signs and digits
-    characters = string.ascii_letters + string.punctuation + string.digits
-    password = first + ''.join(choice(characters) for x in range(randint(min - 1, max - 1)))
+    if min_length > max_length:
+        raise TypeError("Minimum password length must be less or equal than maximum.")
+
+    if upper_case and not lower_case:
+        letters = string.ascii_uppercase
+    elif lower_case and not upper_case:
+        letters = string.ascii_lowercase
+    else:
+        letters = string.ascii_letters
+
+    if first_letter:
+        first = choice(letters)
+    else:
+        first = ''
+
+    valid_characters = letters
+    if punctuation:
+        valid_characters += string.punctuation
+
+    if digits:
+        valid_characters += string.digits
+
+    password = first + ''.join(choice(valid_characters) for x in range(randint(min_length - 1, max_length - 1)))
     return password
--- etlstat/security/test/testPassword.py
+++ etlstat/security/test/testPassword.py
@@ -8,8 +8,8 @@ max_length = 16
 
 class TestPassword(unittest.TestCase):
 
-    def testDatabasePassword(self):
-        pwd = password.database_password(min_length, max_length)
+    def testGeneratePasswordOk(self):
+        pwd = password.generate_password(min_length, max_length, first_letter=True)
         self.assertTrue(len(pwd) >= min_length)
         self.assertTrue(len(pwd) <= max_length)
         self.assertTrue(pwd[:1].isalpha())
