1b2b6c9f53909a646e04eff9cc839b431ae19ed8
root <root@Mugiwara.cantabria.es>
2018-03-21 09:16:03 +0100

    Refactorizado Extractor para mejorar la legibilidad del código

--- etlstat/extractor/extractor.py
+++ etlstat/extractor/extractor.py
@@ -9,99 +9,98 @@ import pandas as pd
 from Levenshtein import ratio
 from etlstat.extractor.pcaxis import *
 
-
-
 conversion_map = {
     'STRING': str,
     'NUMBER': np.float32,
     'DECIMAL': np.float32,
-    'ENTERO': np.int32
+    'INTEGER': np.int32
 }
 
-def similar(a, b):
-    """
-    Args:
-        param a (str): First String to compare.
-        param b (str): Second String to compare.
 
-    Returns:
-        int: Value of similarity between two strings.
+def data_format(dir_path, data, suffix='formato'):
     """
-    return ratio(a, b)
-
-def data_format(dir_path,data,suffix='formato'):
-    """
-    Function that matchs formats files(csv's) with data files in a directory.
+    Function that matches format files(csv's) with data files in a directory.
 
     Args:
         dir_path (str): directory containing files.
         data (str): type of data format(excel,csv,txt)
+        suffix (str): standard for format name
 
     Returns:
-        dict: Excel name and sheetnames as KEYS and data frame as VALUE
+        dict:   Excel name and sheet_names as KEYS and data frame as VALUE if we read excels.
+                Data files names as KEYS and data frame as VALUE in other cases.
     """
-    excel_flag = 0
-    format = '*.[cC][sS][vV]'
-    if(data == 'excel'):
-        max_format = 0
-        excel_flag = 1
-        data = "*.[xX][lL][sS]"
-    if(data == 'txt'):
-        data = "*.[tT][xX][tT]"
-    if(data == 'csv'):
-        data = '*[!'+suffix+'].[cC][sS][vV]'
-        format = '*'+suffix+'.[cC][sS][vV]'
+    format_map = {
+        'excel': '*.[xX][lL][sS]',
+        'txt': '*.[tT][xX][tT]',
+        'csv': '*[!' + suffix + '].[cC][sS][vV]'
+    }
+    # regEx for the name of format files
+    format = '*' + suffix + '*.[cC][sS][vV]'
     assignation_map = {}
-    data_s = []
-    format_s = []
+    # Contains data file names
+    data_list = []
+    # Contains format file names
+    format_list = []
     os.chdir(dir_path)
     for file in os.listdir('.'):
+        # Finds all format files in a directory
         if fnmatch.fnmatch(file, format):
-            format_s.append(file)
-        if fnmatch.fnmatch(file, data):
-            data_s.append(file)
-    csv_list = set(format_s)
-    csv_list = list(csv_list)
-    if len(csv_list) == 0:
-        raise FileNotFoundError ("Not format files found in the directory")
-    keys = set(data_s)
-    keys = list(keys)
-    max_similarity = 0
-    for item in keys:
-        if(excel_flag):
+            format_list.append(file)
+        # Finds all data files in a directory
+        if fnmatch.fnmatch(file, format_map[data]):
+            data_list.append(file)
+    format_list = set(format_list)
+    format_list = list(format_list)
+    if len(format_list) == 0:
+        raise FileNotFoundError("Not format files found in the directory")
+    data_list = set(data_list)
+    data_list = list(data_list)
+    for item in data_list:
+        if (data == 'excel'):
+            max_format = None
+            max_data = None
+            max_format_similarity = 0
+            max_data_similarity = 0
+            # I create a key for each excel
             assignation_map[item] = dict()
             assignation_map[item]['data'] = None
             assignation_map[item]['format'] = None
+            # I add '_datos' and '_hoja' to data string in order to ensure a correct assignment
             data_str = item[:-4] + '_datos' + item[-4:]
-            format_str = item[:-4] + '_hoja' + item[-4:]
-            for element in csv_list:
-                similarity_data = similar(data_str, element)
-                similarity_format = similar(format_str, element)
-                if similarity_data > max_similarity:
-                    max_similarity = similarity_data
-                    aux_data = element
-                if similarity_format > max_format:
-                    max_format = similarity_format
-                    aux_format = element
-            max_similarity = 0
-            max_format = 0
-            assignation_map[item]['data'] = aux_data
-            assignation_map[item]['format'] = aux_format
-            logger.info("Matched data file: " + item + " with: " + assignation_map[item]['data'] + ' and ' + assignation_map[item]['format'])
+            sheet_str = item[:-4] + '_hoja' + item[-4:]
+            for element in format_list:
+                # I check the similarity between format files and data format files
+                similarity_data = ratio(data_str, element)
+                # I check the similarity between format files and sheet format files
+                similarity_sheet = ratio(sheet_str, element)
+                if similarity_data > max_data_similarity:
+                    max_data_similarity = similarity_data
+                    # I take the maximum format data element
+                    max_data = element
+                if similarity_sheet > max_format_similarity:
+                    max_format_similarity = similarity_sheet
+                    # I take the maximum format sheet element
+                    max_format = element
+            assignation_map[item]['data'] = max_data
+            assignation_map[item]['format'] = max_format
+            logger.info("Matched data file: " + item + " with: " + assignation_map[item]['data'] + ' and ' +
+                        assignation_map[item]['format'])
 
         else:
-            for element in csv_list:
-                similarity = similar(item, element)
+            max_similarity = 0
+            max_element = None
+            for element in format_list:
+                similarity = ratio(item, element)
                 if similarity > max_similarity:
                     max_similarity = similarity
-                    aux = element
-            max_similarity = 0
-            assignation_map[item] = aux  # CREATE MAP BETWEEN FILE NAME AND FORMAT NAME
+                    max_element = element
+            assignation_map[item] = max_element
             logger.info("Matched data file: " + item + " with: " + assignation_map[item])
     return assignation_map
 
 
-def excel_in(dir_path, sep=';', encoding='utf-8'):
+def excel_in(dir_path, sep=';', encoding='utf-8', suffix='formato', na_values=None):
     """
     Function that reads excel files in a directory and generates a
     dict with xls names and sheetnames as keys of the dataframe.
@@ -110,145 +109,167 @@ def excel_in(dir_path, sep=';', encoding='utf-8'):
 
     Args:
         dir_path (str): directory containing Excel files.
-        encoding (str): file encoding
+        sep (str): field separator.
+        encoding (str): file encoding.
+        suffix (str): standard for format name.
+        na_values (scalar, str, list-like, or dict) : Additional strings to recognize as NA/NaN.
 
     Returns:
-        dict: Excel name and sheetnames as KEYS and data frame as VALUE
+        dict: Excel name and sheet_names as KEYS and data frame as VALUE
     """
     data = dict()
-    assignation_map = data_format(dir_path,'excel')
+    assignation_map = data_format(dir_path, 'excel', suffix)
     for excel in assignation_map:
         data[excel] = dict()
-        aux = pd.read_csv(dir_path + assignation_map[excel]['format'], sep=sep,
-                encoding=encoding)
-        for line in range(len(aux)):
-            data[excel][aux['SHEET_NAME'][line]] = dict()
-            data[excel][aux['SHEET_NAME'][line]]['START_ROW'] = [aux['START_ROW'][line]]
-            data[excel][aux['SHEET_NAME'][line]]['START_COLUMN'] = [aux['START_COLUMN'][line]]
-            data[excel][aux['SHEET_NAME'][line]]['FIELD_NAME'] = []
-            data[excel][aux['SHEET_NAME'][line]]['DATA_TYPE'] = dict()
-        aux = pd.read_csv(dir_path + assignation_map[excel]['data'], sep=sep,
-                encoding=encoding)
-        for line in range(len(aux)):
-            #print(data[excel][aux['SHEET_NAME'][line]]['DATA_TYPE'])
-            data[excel][aux['SHEET_NAME'][line]]['FIELD_NAME'].append(aux['FIELD_NAME'][line])
-            data[excel][aux['SHEET_NAME'][line]]['DATA_TYPE'][str(aux['FIELD_NAME'][line])] = conversion_map[aux['DATA_TYPE'][line]]
+        # I read sheet format files (csv)
+        file = pd.read_csv(dir_path + assignation_map[excel]['format'], sep=sep,
+                           encoding=encoding)
+        for line in range(len(file)):
+            # I create a key for each sheet_name (data[excel][sheet_name])
+            sheetname = file['SHEET_NAME'][line]
+            data[excel][sheetname] = dict()
+            data[excel][sheetname]['START_ROW'] = file['START_ROW'][line]
+            data[excel][sheetname]['START_COLUMN'] = file['START_COLUMN'][line]
+            data[excel][sheetname]['SKIP_FOOTER'] = file['SKIP_FOOTER'][line]
+            data[excel][sheetname]['FIELD_NAME'] = []
+            data[excel][sheetname]['DATA_TYPE'] = dict()
+        # I read data format files (csv)
+        file = pd.read_csv(dir_path + assignation_map[excel]['data'], sep=sep,
+                           encoding=encoding)
+        # I declare FIELD NAMES and DATA_TYPE(must be a DICT)
+        for line in range(len(file)):
+            sheetname = file['SHEET_NAME'][line]
+            data[excel][sheetname]['FIELD_NAME'].append(file['FIELD_NAME'][line])
+            data[excel][sheetname]['DATA_TYPE'][str(file['FIELD_NAME'][line])] = conversion_map[
+                file['DATA_TYPE'][line]]
 
         for sheet in data[excel]:
-            #print(data[excel][sheet]['DATA_TYPE'])
             data[excel][sheet] = pd.read_excel(open(dir_path + excel, 'rb'),
-                                         sheetname=str(sheet),
-                                         skiprows=data[excel][sheet]['START_ROW'][0]-1,
-                                         names=data[excel][sheet]['FIELD_NAME'],
-                                         dtype=data[excel][sheet]['DATA_TYPE'],
-                                         encoding=encoding)
+                                               sheet_name=str(sheet),
+                                               skiprows=data[excel][sheet]['START_ROW'] - 1,
+                                               skip_footer=data[excel][sheet]['SKIP_FOOTER'],
+                                               names=data[excel][sheet]['FIELD_NAME'],
+                                               dtype=data[excel][sheet]['DATA_TYPE'],
+                                               encoding=encoding,
+                                               na_values=na_values)
             data[excel][sheet].name = sheet
-
 
     return data
 
 
-def csv_in(dir_path, sep=';', encoding='utf-8', suffix='formato',na_values=None):
+def csv_in(dir_path, sep=';', encoding='utf-8', suffix='formato', na_values=None):
     """
-    Function that reads files in a directory filtered by regEx and generates a
-    dict with csv names.
+    Function that reads csv files in a directory and generates a
+    dict with csv names as keys of the dataframe.
+    This method also calls data_format() in order to generate
+    the data frame.
 
     Args:
-        dir_path (str): directory containing Excel files.
-        pattern (str): regEx to filter data names (Avoid adding format extension to regEx)
-        sep (str): field separator character
+        dir_path (str): directory containing Csv files.
+        sep (str): field separator
         encoding (str): file encoding
+        suffix (str): standard for format name
+        na_values (scalar, str, list-like, or dict) : Additional strings to recognize as NA/NaN.
 
     Returns:
         dict: Csv name as KEY and data frame as VALUE
     """
-    data = data_format(dir_path, 'csv', suffix)
-    for csv in data:
+    assignation_map = data_format(dir_path, 'csv', suffix)
+    for csv in assignation_map:
         conversion = dict()
-        data[csv] = pd.read_csv(dir_path + data[csv], sep=sep,
+        assignation_map[csv] = pd.read_csv(dir_path + assignation_map[csv], sep=sep,
                                 encoding=encoding)
-        for line in range(len(data[csv])):
-            conversion[data[csv]['FIELD_NAME'][line]] = conversion_map[data[csv]['DATA_TYPE'][line]]
-        data[csv] = pd.read_csv(dir_path + csv,
+        for line in range(len(assignation_map[csv])):
+            conversion[assignation_map[csv]['FIELD_NAME'][line]] = conversion_map[assignation_map[csv]['DATA_TYPE'][line]]
+        assignation_map[csv] = pd.read_csv(dir_path + csv,
                                 dtype=conversion,
+                                encoding=encoding,
                                 na_values=na_values,
                                 sep=sep)
-        data[csv].name = csv
-    return data
+        assignation_map[csv].name = csv
+    return assignation_map
 
-def pc_axis_in(file_path, sep=",", encoding='windows-1252'):
+
+def pc_axis_in(dir_path, sep=",", encoding='windows-1252'):
     """
     Reads and converts Pc-Axis files to dataframe from URIs listed
     in a CSV file.
 
     Args:
-        file_path (str): extractor with uris file path (including file name).
+        dir_path (str): extractor with uris file path (including file name).
         sep (str): field separator
         encoding (str): file encoding
     Returns:
         dict: URL as KEY and dataframe as VALUE
     """
     pc_axis_dict = {}
-    with open(file_path, "rt") as f:
+    with open(dir_path, "rt") as f:
         reader = csv.reader(f, delimiter=sep)
-
         for row in reader:
-            md, df = from_pc_axis(row[1], encoding)
-            pc_axis_dict[row[0]] = df
+            return_dict = from_pc_axis(row[1], encoding)
+            pc_axis_dict[row[0]] = return_dict['DATA']
 
     return pc_axis_dict
 
-def positional_in(dir_path, sep=';', encoding='utf-8'):
+
+def positional_in(dir_path, sep=';', encoding='windows-1252', suffix='', na_values=None):
     """
     Function that reads files in a directory, generates a correspondence
-    between data files and format files and returning a dict. (MICRODATA)
+    between data files and format files and returning a dict. (MICRODATA).
+    This method also calls data_format() in order to generate
+    the data frame.
 
     Args:
-        dir_path (str): Path of DIR readed.
+        dir_path (str): directory containing Positional files.
+        sep (str): field separator.
+        encoding (str): file encoding.
+        suffix (str): standard for format name
+        na_values (scalar, str, list-like, or dict) : Additional strings to recognize as NA/NaN.
+
 
     Returns:
         dict: Name of data file as KEY and dataframe as VALUE
     """
-    data = data_format(dir_path,'txt')
-    for txt in data:
+    assignation_map = data_format(dir_path, 'txt', suffix)
+    for txt in assignation_map:
         conversion = dict()
-        data[txt] = pd.read_csv(dir_path + data[txt], sep=sep,
+        assignation_map[txt] = pd.read_csv(dir_path + assignation_map[txt], sep=sep,
                                 encoding=encoding)
-        for line in range(len(data[txt])):
-            conversion[data[txt]['FIELD_NAME'][line]] = conversion_map[data[txt]['DATA_TYPE'][line]]
-        data[txt] = pd.read_fwf(dir_path + txt,
-                                widths=data[txt]['LENGTH'].tolist(),
-                                names=data[txt]['FIELD_NAME'].tolist(),
+        for line in range(len(assignation_map[txt])):
+            conversion[assignation_map[txt]['FIELD_NAME'][line]] = conversion_map[assignation_map[txt]['DATA_TYPE'][line]]
+        assignation_map[txt] = pd.read_fwf(dir_path + txt,
+                                widths=assignation_map[txt]['LENGTH'].tolist(),
+                                names=assignation_map[txt]['FIELD_NAME'].tolist(),
                                 dtype=conversion,
-                                nwords=0)
-        data[txt].name = txt
-    return data
+                                nwords=0,
+                                encoding=encoding,
+                                na_values=na_values)
+        assignation_map[txt].name = txt
+    return assignation_map
 
 
-def xml_in(dir_path, pattern=('.xml','.ktr','.kjb','.XML','.KTR','.KJB')):
-
-    # TODO: refactorizar con pattern y fnmatch
+def xml_in(dir_path, pattern='*.[xXkK][mMtTjJ][lLrRbB]'):
     """
     Function that reads files in a directory filtered by regEx and generates a
     dict with xml names.
 
     Args:
-        dir_path (str): Path of DIR readed.
-        reg_ex (str): regEx to filter data names (Avoid adding format extension to regEx)
+        dir_path (str): directory containing XML files.
+        pattern (str): regEx to filter data names
 
     Returns:
         dict: XML name as KEY and etree object as VALUE
     """
-    xmls = []
+    xml_list = []
     os.chdir(dir_path)
     for file in os.listdir('.'):
-        if file.endswith(pattern):
-            xmls.append(file)
-    keys = set(xmls)
-    keys = list(keys)
-    df_dict = dict.fromkeys(keys,'')
-    for i in range(len(keys)):
-        df_dict[keys[i]] = ET.parse(dir_path + keys[i])
+        if fnmatch.fnmatch(file, pattern):
+            xml_list.append(file)
+    xml_list = set(xml_list)
+    xml_list = list(xml_list)
+    df_dict = dict.fromkeys(xml_list, '')
+    for i in range(len(xml_list)):
+        df_dict[xml_list[i]] = ET.parse(dir_path + xml_list[i])
 
     return df_dict
 
--- etlstat/extractor/test/testExtractor.py
+++ etlstat/extractor/test/testExtractor.py
@@ -11,22 +11,22 @@ class TestExtractor(unittest.TestCase):
         string1 = "HOLA"
         string2 = "ADIOS"
         string3 = "HOLA MUNDO"
-        self.assertGreater(similar(string1, string3), similar(string1, string2))
+        self.assertGreater(ratio(string1, string3), ratio(string1, string2))
 
         string1 = "IPI_JUNIO_2017.txt"
         string2 = "JUNIO_IPI_2017.csv"
         string3 = "IPI_Junio_2016.csv"
-        self.assertGreater(similar(string1, string2), similar(string1, string3))
+        self.assertGreater(ratio(string1, string2), ratio(string1, string3))
 
         string1 = "EEE_2015_TERRI_IND_06.txt"
         string2 = "EEE_TERRI.csv"
         string3 = "EEE_UA.csv"
-        self.assertGreater(similar(string1, string2), similar(string1, string3))
+        self.assertGreater(ratio(string1, string2), ratio(string1, string3))
 
         string1 = 'EEE_2015_IDENT_IND_06.TXT'
         string2 = 'EEE_FINAL.csv'
         string3 = 'EEE_IDENT.csv'
-        self.assertGreater(similar(string1, string3), similar(string1, string2))
+        self.assertGreater(ratio(string1, string3), ratio(string1, string2))
 
 
 
@@ -88,35 +88,37 @@ class TestExtractor(unittest.TestCase):
         df = positional_in(dir_path)
 
         post_head = ['NORDEN', 'PI1', 'PI2', 'PI2R', 'PI2N', 'PI21', 'PI31', 'PI41', 'PI42',
-       'PJ1', 'PJ11', 'PJ12', 'PJ13', 'PJ2', 'PJ21', 'PJ22', 'PJ3', 'PJ4',
-       'PJ5', 'PK11', 'PK121', 'PK122', 'PK13', 'PK14', 'PK15', 'PK16', 'PK17',
-       'PK17E', 'PK21', 'PK22', 'PK23', 'PK31', 'PK32', 'PK33', 'PK34', 'PK35',
-       'PK36', 'PK37', 'PK38', 'PK39', 'PK310', 'PK311', 'PK312', 'PK313',
-       'PK314', 'PK315', 'PK316', 'PK316E', 'PL11', 'PL12', 'PL13', 'PL14',
-       'PL21', 'PL22', 'PL23', 'PL24', 'PL31', 'PL32', 'PL33', 'PL34', 'PL41',
-       'PL42', 'PL43', 'PL44', 'PL51', 'PL52', 'PL53', 'PL54', 'PL5E', 'PM1',
-       'PM21', 'PM22', 'PM3', 'PM4']
+                     'PJ1', 'PJ11', 'PJ12', 'PJ13', 'PJ2', 'PJ21', 'PJ22', 'PJ3', 'PJ4',
+                     'PJ5', 'PK11', 'PK121', 'PK122', 'PK13', 'PK14', 'PK15', 'PK16', 'PK17',
+                     'PK17E', 'PK21', 'PK22', 'PK23', 'PK31', 'PK32', 'PK33', 'PK34', 'PK35',
+                     'PK36', 'PK37', 'PK38', 'PK39', 'PK310', 'PK311', 'PK312', 'PK313',
+                     'PK314', 'PK315', 'PK316', 'PK316E', 'PL11', 'PL12', 'PL13', 'PL14',
+                     'PL21', 'PL22', 'PL23', 'PL24', 'PL31', 'PL32', 'PL33', 'PL34', 'PL41',
+                     'PL42', 'PL43', 'PL44', 'PL51', 'PL52', 'PL53', 'PL54', 'PL5E', 'PM1',
+                     'PM21', 'PM22', 'PM3', 'PM4']
 
-        post_type = ['O', 'float32', 'float32', 'O', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32']
+        post_type = ['O', 'float32', 'float32', 'O', 'O', 'float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32','float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'O',
+                     'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32','float32', 'float32', 'float32','float32', 'float32', 'float32', 'float32', 'float32',
+                     'float32', 'float32', 'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32',
+                     'float32']
 
         tec_head = ['NORDEN', 'KI111', 'KI112', 'KI113', 'KI114', 'KI115', 'KI116', 'KI121',
-       'KI122', 'KI123', 'KI124', 'KI125', 'KI126', 'KI127', 'KI128', 'KI129',
-       'I129E', 'KI131', 'KI132', 'KI133', 'KI134', 'KI134E', 'KI141', 'KI142',
-       'KI143', 'KI144', 'KI145', 'KI145E', 'KI15', 'KI16', 'KI16E', 'KI21',
-       'KI22', 'KI23', 'KI24', 'KI25', 'KI26', 'KI27', 'KI28', 'KI29', 'KI210',
-       'KI211', 'KI212', 'KI212E']
+                    'KI122', 'KI123', 'KI124', 'KI125', 'KI126', 'KI127', 'KI128', 'KI129',
+                    'I129E', 'KI131', 'KI132', 'KI133', 'KI134', 'KI134E', 'KI141', 'KI142',
+                    'KI143', 'KI144', 'KI145', 'KI145E', 'KI15', 'KI16', 'KI16E', 'KI21',
+                    'KI22', 'KI23', 'KI24', 'KI25', 'KI26', 'KI27', 'KI28', 'KI29', 'KI210',
+                    'KI211', 'KI212', 'KI212E']
 
-        tec_type = ['O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'O', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'O', 'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
-        'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'O']
-
-
+        tec_type = ['O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                    'float32', 'float32','float32', 'float32', 'float32', 'float32', 'float32', 'O', 'float32',
+                    'float32', 'float32', 'float32', 'O', 'float32', 'float32','float32', 'float32', 'float32', 'O',
+                    'float32', 'float32', 'O', 'float32', 'float32', 'float32', 'float32', 'float32', 'float32',
+                    'float32', 'float32', 'float32', 'float32', 'float32', 'float32', 'O']
 
         assert (df['AEREO_SER_06_15.TXT'].shape[0] == 2)
         assert (df['AEREO_SER_06_15.TXT'].shape[1] == 39)
