2e8d820d79d21b57794398595edcffdb9887ea1a
Alberto Lezcano Lastra <lezcano_a@cantabria.es>
2018-03-21 09:37:19 +0100

    Update package version before upload to PyPi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='etlstat',
-    version='0.1.1',
+    version='0.1.2',
     author='Instituto Cántabro de Estadística',
     author_email='icane.datos@cantabria.es',
     packages=find_packages(),
     url='https://github.com/icane/etlstat.git',
-    download_url='https://github.com/icane/etlstat/archive/0.1.1.tar.gz',
+    download_url='https://github.com/icane/etlstat/archive/0.1.2.tar.gz',
     license='Apache License 2.0',
     description='Python package that contains extract-transform-load utils for statistical offices data processing',
     long_description=open('README.md').read(),
