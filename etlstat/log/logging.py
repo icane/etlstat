f06028fe16da704077fcbec573c12b7ef7813197
rsj9999 <rsj9999@gitlab.com>
2018-06-08 09:16:31 +0200

    Añade módulo logging.py para la clase LoggerMixin en etlstat/log/
    Añade esta clase para obtener un logging de las tareas que se ejecutan
    La idea es encapsular esta clase en otras clases que se utilicen

---
+++ etlstat/log/logging.py
@@ -0,0 +1,10 @@
+import logging
+
+class LoggerMixin(object):
+    @property
+    def logger(self):
+        name = '.'.join([
+            self.__module__,
+            self.__class__.__name__
+        ])
+        return logging.getLogger(name)
