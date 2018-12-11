c5ab527316661794198eed7788cf81e9cde2c510
Alberto Lezcano Lastra <lezcano_a@cantabria.es>
2018-12-11 18:20:12 +0100

    Refactor Oracle class

--- etlstat/database/oracle.py
+++ etlstat/database/oracle.py
@@ -1,5 +1,6 @@
+# coding: utf-8
 """
-    Oracle
+This module manages Oracle primitives.
 
     Date:
         23/01/2018
@@ -8,17 +9,19 @@
         lla11358
 
     Version:
-        Alpha
+        0.1
 
     Notes:
 
 """
 
 import csv
-import os
 import logging
+import os
 import pandas as pd
-from sqlalchemy import create_engine
+import shlex
+import subprocess
+from sqlalchemy import create_engine, text, MetaData, Table
 from sqlalchemy.exc import DatabaseError
 
 logging.basicConfig(level=logging.INFO)
@@ -27,48 +30,81 @@ LOGGER = logging.getLogger(__name__)
 
 class Oracle:
     """
-    Oracle database connection management
+    Manages connections to Oracle databases.
+
+        Oracle class offers some helper methods that encapsulate primitive
+        logic to interactuate with the database: insert/upsert, execute,
+        drop, etc.
+        Some primitives are not included because they can be handled more
+        properly with sqlalchemy.
+
     """
 
-    engine = None
-
-    conversion_map = {
-        'object': 'VARCHAR2(256)',
-        'int32': 'INTEGER',
-        'int64': 'INTEGER',
-        'float32': 'NUMBER',
-        'float64': 'NUMBER'
-    }
-
-    def __init__(self, user, password, host, port,
-                 service_name, encoding='utf8'):
-        conn_string = f'''oracle+cx_oracle://{user}:{password}@{host}:{port}
-                       /?service_name={service_name}'''
-        self.engine = create_engine(
-            conn_string,
-            encoding=encoding,
-            coerce_to_unicode=True,
-            coerce_to_decimal=False)
-
-    def execute_sql(self, sql):
+    def __init__(
+            self, user, password, host,
+            port, service_name, encoding='utf8'):
         """
-        Executes a DDL or DML SQL statement. Implements a transaction.
+        Initialize the database connection and other relevant data.
 
-        Args:
-            sql (str): SQL statement
+            Args:
+              user(string): database user to connect to the schema.
+              password(string): database password of the user.
+              host(string): database management system host.
+              port(string): tcp port where the database is listening.
+              service_name(string): Oracle instance name.
+              encoding (string): Charset encoding.
+        """
+        self.conn_string = "oracle+cx_oracle://{0}:{1}@{2}:{3}/{4}".format(
+            user,
+            password,
+            host,
+            port,
+            service_name)
+        self.engine = create_engine(self.conn_string,
+                                    encoding=encoding,
+                                    coerce_to_unicode=True,
+                                    coerce_to_decimal=False)
+        self.schema = user.upper()
 
-        Returns:
-            result_set (dataframe)
+    def get_table(self, table_name, schema=None):
+        """
+        Get a database table into a sqlalchemy Table object.
+
+            Args:
+                table_name(string): name of the database table to map.
+                schema(string): name of the schema to which the table belongs.
+                    Defaults to the selected database in the connection.
+            Returns:
+                table(Table): sqlalchemy Table object referencing the specified
+                    database table.
+
+        """
+        meta = MetaData(bind=self.engine,
+                        schema=schema if schema else self.schema)
+        return Table(table_name, meta, autoload=True,
+                     autoload_with=self.engine)
+
+    def execute(self, sql, **kwargs):
+        """
+        Execute a DDL or DML SQL statement.
+
+            Args:
+                sql: SQL statement
+            Returns:
+                result_set(Dataframe):
+
         """
         connection = self.engine.connect()
         trans = connection.begin()
         result_set = pd.DataFrame()
         try:
-            result = connection.execute(sql)
+            result = connection.execute(text(sql), **kwargs)
             trans.commit()
             if result.returns_rows:
                 result_set = pd.DataFrame(result.fetchall())
                 result_set.columns = result.keys()
+                LOGGER.info('Number of returned rows: %s',
+                            str(len(result_set.index)))
         except DatabaseError as db_error:
             LOGGER.error(db_error)
             raise
@@ -76,45 +112,43 @@ class Oracle:
             connection.close()
         return result_set
 
-    def execute_query(self, sql):
+    def drop(self, table_name, schema=None):
         """
-        Executes a DDL or DML SQL statement. Implements a transaction.
+        Drop a table from the database.
 
         Args:
-            sql (str): SQL statement
+          table_name(str): name of the table to drop.
 
-        Returns:
-            result_set (dataframe)
+        Returns: nothing.
+
         """
-        connection = self.engine.connect()
-        result_set = pd.DataFrame()
-        try:
-            result = connection.execute(sql)
-            result_set = pd.DataFrame(result.fetchall())
-            result_set.columns = result.keys()
-        except DatabaseError as db_error:
-            LOGGER.error(db_error)
-            raise
-        finally:
-            connection.close()
-        return result_set
+        if not schema:
+            schema = self.schema
+        db_table = self.get_table(table_name, schema)
+        db_table.drop(self.engine, checkfirst=True)
+        LOGGER.info('Table %s.%s successfully dropped.', schema, table_name)
+
+        # Placeholders can only represent VALUES. You cannot use them for
+        # sql keywords/identifiers.
 
     @staticmethod
-    def bulk_insert(user,
-                    password,
-                    host,
-                    port,
-                    service_name,
-                    schema,
-                    table,
-                    output_path,
-                    os_path,
-                    os_ld_library_path,
-                    mode="APPEND"):
+    def load_data(
+        user,
+        password,
+        host,
+        port,
+        service_name,
+        schema,
+        table,
+        output_path,
+        os_path,
+        os_ld_library_path,
+        mode="APPEND"):
         """
-        Generates control and data files for Oracle SQL Loader by extracting
-        field names and data values from a Pandas DataFrame. Requires Oracle
-        Instant Client and Tools installed in the workstation.
+        Load a dataframe into a table via Oracle SQL Loader.
+
+        Extracts field names and data values from a Pandas DataFrame.
+        Requires Oracle Instant Client and Tools installed in the workstation.
         Destination table must exist in the database.
         Usage of SQL Loader:
 
@@ -129,39 +163,36 @@ class Oracle:
             service_name (str): Oracle instance service name
             schema (str): database schema
             table (pandas DataFrame): dataframe with the same name and column
-                                      labels as the table in which it's going
-                                      to be loaded. It must be filled with
-                                      data rows.
+                labels as the table in which it's going to be loaded.
+                It must be filled with data rows.
             output_path (str): path for output data files
             os_path (str): PATH environment variable
             os_ld_library_path (str): LD_LIBRARY_PATH environment variable
             mode (str): insertion mode: APPEND | REPLACE | TRUNCATE
 
-        Returns:
-            True if success or False
         """
-
         columns = ",".join(table.columns.values.tolist())
 
         # control file
-        ctl_file = open(output_path + table.name + '.ctl', mode='w',
+        ctl_file = open('{0}{1}.ctl'.format(output_path, table.name),
+                        mode='w',
                         encoding='utf8')
         ctl_header = """LOAD DATA\n""" + \
                      """CHARACTERSET UTF8\n""" + \
-                     """INFILE '""" + output_path + table.name + '.dat' + \
-                     """'\n""" + mode + """\n""" + \
-                     """INTO TABLE """ + schema + """.""" + table.name + \
-                     """\n""" + \
+                     """INFILE '{0}{1}.dat'\n""".format(
+                         output_path, table.name) + \
+                     """{0}\n""".format(mode) + \
+                     """INTO TABLE {0}.{1}\n""".format(schema, table.name) + \
                      """FIELDS TERMINATED BY ';' """ + \
                      """OPTIONALLY ENCLOSED BY '\"'\n""" + \
                      """TRAILING NULLCOLS\n""" + \
-                     """(""" + columns + """)"""
+                     """({0})""".format(columns)
 
         ctl_file.write(ctl_header)
         ctl_file.close()
 
         # data file
-        table.to_csv(output_path + table.name + '.dat',
+        table.to_csv('{0}{1}.dat'.format(output_path, table.name),
                      sep=';',
                      header=False,
                      index=False,
@@ -170,14 +201,19 @@ class Oracle:
                      encoding='utf-8'
                      )
 
+        # set environment variables
+        env = os.environ.copy()
+        env['PATH'] = os_path
+        env['LD_LIBRARY_PATH'] = os_ld_library_path
+        # generate SQL Loader arguments
+        os_command = "sqlldr {0}/{1}@{2}:{3}/{4} ".format(
+            user, password, host, port, service_name)
+        os_command += \
+            "control='{0}{1}.ctl' log='{0}{1}.log' bad='{0}{1}.bad'".format(
+                output_path, table.name)
+        args = shlex.split(os_command)
         # execution of Oracle SQL Loader
-        os_command = "export PATH=" + os_path + "; export LD_LIBRARY_PATH=" + \
-                     os_ld_library_path + "; "
-        os_command += "sqlldr " + user + "/" + password + "@" + host + ":" + \
-                      port + "/" + service_name + " "
-        os_command += "control='" + output_path + table.name + ".ctl' "
-        os_command += "log='" + output_path + table.name + ".log' "
-        os_command += "bad='" + output_path + table.name + ".bad'"
-        print(os_command)
-
-        return os.system(os_command)
+        try:
+            subprocess.Popen(args, env=env)
+        except subprocess.SubprocessError as sproc_error:
+            LOGGER.error(sproc_error)
--- etlstat/database/test/test_oracle.py
+++ etlstat/database/test/test_oracle.py
@@ -1,67 +1,57 @@
 # -*- coding: utf-8 -*-
 
-"""
-Unit tests for oracle database module.
-"""
+"""Unit tests for oracle database module."""
 
 import os
 import unittest
 import pandas as pd
+from sqlalchemy.exc import InvalidRequestError
 from etlstat.database.oracle import Oracle
 
 os.environ['NLS_LANG'] = '.AL32UTF8'
 
 
 class TestOracle(unittest.TestCase):
-    """Testing methods for Oracle class"""
+    """Testing methods for Oracle class."""
 
-    def setUp(self):
-        """Sets up test variables"""
+    user = 'test'
+    password = 'password'
+    host = 'localhost'
+    port = '1521'
+    service_name = 'xe'
+    conn_params = [user, password, host, port, service_name]
+    output_path = os.getcwd() + '/etlstat/database/test/'
+    os_path = '/usr/local/bin:/usr/bin:/bin:' \
+        '/var/opt/oracle/instantclient_18_3'
+    os_ld_library_path = '/var/opt/oracle/instantclient_18_3'
 
-        self.user = 'system'
-        self.password = 'oracle'
-        self.host = 'localhost'
-        self.port = '1521'
-        self.service_name = 'xe'
-        self.conn_params = [self.user, self.password, self.host, self.port,
-                            self.service_name]
-        self.ora_conn = Oracle(*self.conn_params)
+    @classmethod
+    def setUpClass(cls):
+        """Set up test variables."""
+        user = 'system'
+        password = 'oracle'
+        host = 'localhost'
+        port = '1521'
+        service_name = 'xe'
+        conn_params = [user, password, host, port, service_name]
+        my_conn = Oracle(*conn_params)
         sql = "DROP USER TEST CASCADE"
-        status = self.ora_conn.execute_sql(sql)[0]
-        self.flag1 = status
-        sql = "CREATE USER test IDENTIFIED BY test " \
+        my_conn.execute(sql)
+        sql = "CREATE USER test IDENTIFIED BY password " \
               "DEFAULT TABLESPACE USERS TEMPORARY TABLESPACE TEMP"
-        status = self.ora_conn.execute_sql(sql)[0]
-        self.flag2 = status
+        my_conn.execute(sql)
         sql = "ALTER USER test QUOTA UNLIMITED ON USERS"
-        status = self.ora_conn.execute_sql(sql)[0]
-        self.flag3 = status
+        my_conn.execute(sql)
         sql = "GRANT CONNECT, RESOURCE TO test"
-        status = self.ora_conn.execute_sql(sql)[0]
-        self.flag4 = status
-        self.output_path = os.getcwd() + '/etlstat/database/test/'
-        self.os_path = '/usr/local/bin:/usr/bin:/bin:' \
-                       '/var/opt/oracle/instantclient_18_3'
-        self.os_ld_library_path = '/var/opt/oracle/instantclient_18_3'
-        self.schema = 'test'
-        self.user = 'test'
-        self.password = 'test'
-        self.conn_params = [self.user, self.password, self.host, self.port,
-                            self.service_name]
-
-    def test_connection(self):
-        """ Check connection with Oracle database"""
-
-        self.assertTrue(self.flag3)
-        self.assertTrue(self.flag4)
+        my_conn.execute(sql)
 
     def test_init(self):
-        """ Check connection with Oracle database"""
+        """Check connection with Oracle database."""
         self.assertEqual(str(Oracle(*self.conn_params).engine),
                          "Engine(oracle+cx_oracle://test:***@localhost:1521"
                          "/?service_name=xe)")
 
-    def test_execute_sql(self):
+    def test_execute(self):
         """ Check if different queries are correctly executed."""
         ora_conn = Oracle(*self.conn_params)
         sql = "CREATE TABLE test_sql (id integer, column1 varchar2(100), " \
@@ -91,7 +81,87 @@ class TestOracle(unittest.TestCase):
         status, result = ora_conn.execute_sql(sql)
         self.assertTrue(status)
 
-    def test_bulk_insert(self):
+    def test_select(self):
+        """ Check select statement using sqlalchemy"""
+        my_conn = MySQL(*self.conn_params)
+        table_name = "inf_schema"
+        inf_schema = my_conn.get_table(table_name)
+        # SELECT * FROM inf_schema
+        # WHERE table_name like 'INNO%' AND avg_row_length > 100
+        results = my_conn.engine.execute(select('*')
+                                         .where(inf_schema.c.table_name
+                                                .like('INNO%'))
+                                         .where(inf_schema.c.avg_row_length >
+                                                100)
+                                         .select_from(inf_schema)).fetchall()
+        table_df = pd.DataFrame(results)
+        self.assertGreaterEqual(len(table_df), 6)
+
+    def test_get_table(self):
+        """Check get table from the database using SqlAlchemy."""
+        my_conn = Oracle(*self.conn_params)
+        inf_schema = my_conn.get_table('inf_schema')  # GET TABLE example
+        row_count = my_conn.engine.scalar(
+            select([func.count('*')]).select_from(inf_schema)
+        )
+        # The select.columns parameter is not available in the method form of
+        # select(), e.g. FromClause.select().
+        # See https://docs.sqlalchemy.org/en/latest/core/selectable.html#
+        # sqlalchemy.sql.expression.FromClause.select
+        my_conn.engine.execute(
+            select([inf_schema.c.table_name]).select_from(inf_schema))
+        self.assertEqual(row_count, 295)
+
+    def test_create(self):
+        """ Check create table using sqlalchemy """
+        Base = declarative_base()
+        my_conn = MySQL(*self.conn_params)
+
+        # table creation can be done via execute() + raw SQL or using this:
+        class Table2(Base):
+            """ Auxiliary sqlalchemy table model for the tests"""
+            __tablename__ = 'table2'
+
+            column_int = Column(Integer)
+            column_string = Column(String(20))
+            column_float = Column(Float)
+            column_datetime = Column(DateTime)
+            column_boolean = Column(Boolean)
+            id = Column(Integer, primary_key=True)
+
+        Table2.__table__.create(bind=my_conn.engine)
+        table2 = my_conn.get_table('table2')
+        self.assertEqual(table2.c.column_datetime.name, 'column_datetime')
+        self.assertEqual(len(table2.c), 6)
+        my_conn.drop('table2')
+
+    def test_drop(self):
+        """ Check drop for an existing table """
+        my_conn = Oracle(*self.conn_params)
+        sql = "CREATE TABLE table1 (id integer, column1 varchar(100), " \
+            "column2 double)"
+        my_conn.execute(sql)
+        my_conn.get_table('table1')
+        my_conn.drop('table1')  # DROP example
+        with self.assertRaises(InvalidRequestError):
+            my_conn.get_table('table1')
+
+    def test_delete(self):
+        """ Check delete rows from table."""
+        data_columns = ['column_int', 'column_string', 'column_float']
+        data_values = [[1, 'string1', 456.956], [2, 'string2', 38.905]]
+        data = pd.DataFrame(data_values, columns=data_columns)
+        data.name = 'test_delete'
+        my_conn = MySQL(*self.conn_params)
+        my_conn.insert(data)
+        table = my_conn.get_table(data.name)
+        expected = 2
+        current = my_conn.engine.scalar(
+            select([func.count('*')]).select_from(table)
+        )
+        self.assertEqual(current, expected)
+
+    def test_load_data(self):
         """Check if a bulk insert with sql loader is correctly executed."""
         source_file = self.output_path + '01001.csv'
         table_name = 'px_01001'
