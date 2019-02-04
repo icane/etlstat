7447ce11c8a30de2ff48b9053ef80d54a45ef0a2
Miguel Expósito <exposito_m@cantabria.es>
2019-02-04 16:52:17 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='etlstat',
-    version='0.4.2',
+    version='0.4.3',
     author='Instituto Cántabro de Estadística',
     author_email='icane@cantabria.es',
     packages=find_packages(),
