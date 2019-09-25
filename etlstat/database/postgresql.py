a2cd6a4f0b145b074d1d329225a679cb20315186
Alberto Lezcano <alberto@ayax.estudio.net>
2019-09-25 23:48:44 +0200

    Add minimal support for Postgresql database

---
+++ etlstat/database/postgresql.py
@@ -0,0 +1,95 @@
+"""
+This module manages Postgresql primitives.
+
+    Date:
+        25/09/2019
+
+    Author:
+        lla11358
+
+    Version:
+        0.1
+
+    Notes:
+
+"""
+
+import os
+import logging
+import sqlparse
+from sqlalchemy import create_engine, text, select, func, MetaData, Table
+from sqlalchemy.exc import DatabaseError
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+class Postgresql:
+    """
+    Manage connections to Postgresql databases.
+
+    Postgresql class offers some helper methods that encapsulate primitive logic
+    to interactuate with the database: insert/upsert, execute, drop, etc.
+    Some primitives are not included because they can be handled more
+    properly with sqlalchemy.
+
+    """
+
+    def __init__(self, *conn_params):
+        """
+        Initialize the database connection and other relevant data.
+
+        Args:
+            *conn_params: list with the following connection parameters:
+                user(string): database user to connect to the schema.
+                password(string): database password of the user.
+                host(string): database management system host.
+                port(string): tcp port where the database is listening.
+                database(string): schema or database name.
+
+        """
+        # connection string in sqlalchemy format
+        self.conn_string = f'''postgresql://{conn_params[0]}:''' + \
+            f'''{conn_params[1]}@{conn_params[2]}:''' + \
+            f'''{str(conn_params[3])}/{conn_params[4]}'''
+        self.engine = create_engine(self.conn_string)
+        self.database = conn_params[4]
+
+    def execute(self, sql, **kwargs):
+        """
+        Execute a DDL or DML SQL statement.
+
+            Args:
+                sql (string): SQL statement(s) separated by semicolons (;)
+                kwargs (dict): optional statement named parameters
+            Returns:
+                results (list): list of dataframes. Non-SELECT statements
+                    returns empty dataframes.
+
+        """
+        results = []
+        statements = sqlparse.split(sql)
+        connection = self.engine.connect()
+        # begin transaction
+        trans = connection.begin()
+        try:
+            for statement in statements:
+                result_set = pd.DataFrame()
+                result = connection.execute(
+                    text(statement.strip(';')), **kwargs)
+                if result.returns_rows:
+                    result_set = pd.DataFrame(result.fetchall())
+                    result_set.columns = result.keys()
+                    LOGGER.info('Number of returned rows: %s',
+                                str(len(result_set.index)))
+                results.append(result_set)
+            # end transaction
+            trans.commit()
+        except DatabaseError as db_error:
+            trans.rollback()
+            LOGGER.error(db_error)
+            raise
+        finally:
+            connection.close()
+        return results
---
+++ etlstat/database/test/test_postgresql.py
@@ -0,0 +1,98 @@
+"""Unit tests for Postgresql database module."""
+
+import os
+import unittest
+import pandas as pd
+
+from etlstat.database.postgresql import Postgresql
+from etlstat.text import utils
+
+from pyaxis import pyaxis
+
+from sqlalchemy import (Boolean, Column, DateTime, Float, Integer, String,
+                        func, select)
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class TestPostgresql(unittest.TestCase):
+    """Testing methods for Postgresql class."""
+
+    user = 'test'
+    password = 'password'
+    host = '127.0.0.1'
+    port = '5432'
+    database = 'postgres'
+    conn_params = [user, password, host, port, database]
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up test variables."""
+        user = 'postgres'
+        password = 'password'
+        host = '127.0.0.1'
+        port = '5432'
+        database = 'postgres'
+        conn_params = [user, password, host, port, database]
+        ddl = "DROP SCHEMA IF EXISTS test CASCADE"
+        pg_conn = Postgresql(*conn_params)
+        pg_conn.execute(ddl)
+        ddl = "DROP USER IF EXISTS test"
+        pg_conn.execute(ddl)
+        ddl = "CREATE SCHEMA test"
+        pg_conn.execute(ddl)
+        sql = "CREATE TABLE IF NOT EXISTS test.inf_schema AS " \
+              "SELECT table_catalog, table_schema, " \
+              "table_name, table_type " \
+              "FROM information_schema.tables"
+        pg_conn.execute(sql)
+        ddl = "CREATE USER test WITH ENCRYPTED PASSWORD 'password'"
+        pg_conn.execute(ddl)
+        ddl = "GRANT ALL PRIVILEGES ON SCHEMA test TO test"
+        pg_conn.execute(ddl)
+
+    def test_init(self):
+        """Check connection with Postgresql database."""
+        self.assertEqual(str(Postgresql(*self.conn_params).engine),
+                         "Engine(postgresql://test:***@127.0.0.1:"
+                         "5432/postgres)")
+
+    def test_execute(self):
+        """Check execute method launching arbitrary sql queries."""
+        pg_conn = Postgresql(*self.conn_params)
+        sql = f'''CREATE TABLE table1 (id integer, column1 varchar(100),
+                  column2 float)'''
+        pg_conn.execute(sql)
+        sql = "INSERT INTO table1 (id, column1, column2) " \
+              "VALUES (1, 'Varchar text (100 char)', 123456789.0123456789)"
+        pg_conn.execute(sql)
+        result = pg_conn.execute("SELECT * FROM table1")
+        self.assertEqual('Varchar text (100 char)', result[0]['column1'][0])
+        query = 'SELECT * FROM table1 ORDER BY id'
+        result = pg_conn.execute(query)
+        expected = 1
+        current = len(result[0].index)
+        self.assertEqual(expected, current)
+        pg_conn.execute('DROP TABLE table1')
+
+
+    def test_execute_multiple(self):
+        """Check if multiple SQL statements are correctly executed."""
+        pg_conn = Postgresql(*self.conn_params)
+        sql = f"""CREATE TABLE table1 (id integer, column1 varchar(100),
+              column2 float);
+              INSERT INTO table1 (id, column1, column2)
+              VALUES (1, 'Varchar; text; (100 char)',
+              123456789.012787648484859);
+              INSERT INTO table1 (id, column1, column2)
+              VALUES (2, 'Varchar; text; (100 char)',
+              -789.0127876);
+              SELECT id, column2 FROM table1;"""
+        result = pg_conn.execute(sql)
+        self.assertEqual(len(result), 4)
+        self.assertEqual(len(result[3].index), 2)
+        pg_conn.execute('DROP TABLE table1')
+
+
+if __name__ == '__main__':
+    unittest.main()
